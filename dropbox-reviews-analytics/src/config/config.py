b202fc451a328bb5f9012500b4857b894b16e367
Abhiraj Adhikary <abhirajadhikary06@gmail.com>
2024-12-27 11:11:29 +0530

    added updates

--- dropbox-reviews-analytics/src/config/config.py
+++
@@ -1,8 +0,0 @@
-# config/config.py
-from dotenv import load_dotenv
-import os
-load_dotenv()
-
-MOTHERDUCK_TOKEN = os.getenv('MOTHERDUCK_TOKEN')
-if not MOTHERDUCK_TOKEN:
-    raise ValueError("Please set MOTHERDUCK_TOKEN environment variable")
